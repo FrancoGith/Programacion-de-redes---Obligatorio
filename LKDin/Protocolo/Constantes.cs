bacc0191d6a05aa2275f5ca58ad77d35560ceaa1
mmazziotti <matimazziotti@gmail.com>
2022-09-16 18:12:19 -0300

    creado protocolo envio y constantes

---
+++ LKDin/Protocolo/Constantes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Protocolo
+{
+    internal class Constantes
+    {
+        public const int LargoCodigo = 2;
+        public const int LargoLongitudMensaje = 4;
+
+
+        public const int LargoHeader = LargoCodigo + LargoLongitudMensaje;
+    }
+}
