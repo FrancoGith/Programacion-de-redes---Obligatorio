96f6bbe8a1661da0e302bf419bce083bf0db2f68
Franco Daneri <francodaneri0712@gmail.com>
2022-11-24 10:06:19 -0300

    ProgramaServidor.cs Matias

--- LKDin2/LKDin/LKDin/GrpcServerProgram/Servidor/ProgramaServidor.cs
+++ LKDin2/LKDin/LKDin/GrpcServerProgram/Servidor/ProgramaServidor.cs
@@ -1,18 +1,18 @@
 ﻿using Dominio;
 using Dominio.Mensajes;
-using Google.Protobuf.Collections;
+using GrpcServerProgram.Servidor;
 using Protocolo;
 using Protocolo.ManejoArchivos;
 using System.Data;
 using System.Net;
 using System.Net.Sockets;
 
-namespace GrpcServerProgram.Servidor
+namespace Servidor
 {
     class ProgramaServidor
     {
         static readonly SettingsManager settingsManager = new SettingsManager();
-        private static DatosServidor datosServidor;
+        private static DatosServidor datosServidor = new DatosServidor();
         private readonly TcpListener tcpListener;
         private bool aceptandoConexiones;
 
@@ -24,34 +24,30 @@ namespace GrpcServerProgram.Servidor
 
         public ProgramaServidor()
         {
-            datosServidor = DatosServidor.GetInstancia();
             datosServidor.AgregarUsuario("U1", "U1");
             datosServidor.AgregarUsuario("U2", "U2");
             datosServidor.AgregarUsuario("U3", "U3");
 
-            datosServidor.AgregarPerfilTrabajo("U1", new List<string>() { "C#", "Java" }, "Programador");
-            datosServidor.AgregarPerfilTrabajo("U2", new List<string>() { "Python" }, "Backend");
-            datosServidor.AgregarPerfilTrabajo("U3", new List<string>() { "Java" }, "QA");
+            datosServidor.AgregarPerfilTrabajo(datosServidor.GetUsuario("U1"), new List<string>() { "C#", "Java" }, "Programador");
+            datosServidor.AgregarPerfilTrabajo(datosServidor.GetUsuario("U2"), new List<string>() { "Python" }, "Backend");
+            datosServidor.AgregarPerfilTrabajo(datosServidor.GetUsuario("U3"), new List<string>() { "Java" }, "QA");
 
             var endpoint = new IPEndPoint(IPAddress.Parse(serverIP), serverPort);
             tcpListener = new TcpListener(endpoint);
             aceptandoConexiones = true;
-
 
             Console.WriteLine($"Servidor en la dirección {serverIP} y puerto {serverPort}");
         }
 
         public async Task ComenzarRecibirConexiones()
         {
-
             tcpListener.Start(cantidadClientes);
             while (aceptandoConexiones)
             {
                 try
                 {
-
-                    var tcpClientSocket = await tcpListener.AcceptTcpClientAsync();
-                    var task = Task.Run(async () => await ManejarCliente(tcpClientSocket));
+                    var tcpClientSocket = await tcpListener.AcceptTcpClientAsync().ConfigureAwait(false);
+                    Task task = Task.Run(async () => await ManejarCliente(tcpClientSocket).ConfigureAwait(false));
                 }
                 catch (SocketException)
                 {
@@ -81,9 +77,6 @@ namespace GrpcServerProgram.Servidor
                         {
                             case 1:
                                 await LogIn(manejoDataSocket, mensajeUsuario);
-                                break;
-                            case 2:
-                                Logout(mensajeUsuario);
                                 break;
                             case 10:
                                 await AltaDeUsuario(manejoDataSocket, mensajeUsuario);
@@ -131,18 +124,24 @@ namespace GrpcServerProgram.Servidor
         static async Task AltaDeUsuario(ManejoStreamsHelper manejoDataSocket, string mensajeUsuario)
         {
             string[] datos = mensajeUsuario.Split(Constantes.CaracterSeparador);
+            string log = string.Empty;
 
             if (datosServidor.GetUsuario(datos[0]) != null)
             {
                 await EnviarMensajeCliente(manejoDataSocket, "Usuario existente, intente con otro nombre.", "12");
-                Console.WriteLine("No se ha ingresado el usuario porque ya existia");
+                log = "No se ha ingresado el usuario porque ya existia";
+                Console.WriteLine(log);
+                
             }
             else
             {
                 datosServidor.AgregarUsuario(datos[0], datos[1]);
                 await EnviarMensajeCliente(manejoDataSocket, "Usuario creado", "11");
-                Console.WriteLine("Se ha creado un nuevo usuario");
+                log = "Se ha creado un nuevo usuario";
+                Console.WriteLine(log);
             }
+
+            LogHelper.PublishLog("Creación usuario", log);
         }
 
         private static async Task AltaDePerfilDeTrabajo(ManejoStreamsHelper manejoDataSocket, string mensajeUsuario)
@@ -153,28 +152,35 @@ namespace GrpcServerProgram.Servidor
             {
                 Usuario usuario = datosServidor.GetUsuario(datos[0]);
                 PerfilTrabajo perfilTrabajo = datosServidor.GetPerfilTrabajo(datos[0]);
+                string log = string.Empty;
+
                 if (usuario != null)
                 {
                     if (perfilTrabajo != null)
                     {
                         await EnviarMensajeCliente(manejoDataSocket, "Perfil de trabajo existente para este usuario", "22");
-                        Console.WriteLine("Perfil de trabajo existente para este usuario");
+                        log = "Perfil de trabajo existente para este usuario";
+                        Console.WriteLine(log);
                     }
                     else
                     {
-                        List<string> habilidades = new(datos[1].Split(Constantes.CaracterSeparadorListas));
+                        List<string> habilidades = new List<string>(datos[1].Split(Constantes.CaracterSeparadorListas));
                         string descripcion = datos[2];
 
-                        datosServidor.AgregarPerfilTrabajo(usuario.Username, habilidades, descripcion);
+                        datosServidor.AgregarPerfilTrabajo(usuario, habilidades, descripcion);
 
                         await EnviarMensajeCliente(manejoDataSocket, "Perfil de trabajo creado", "23");
-                        Console.WriteLine("Se ha creado un nuevo perfil de trabajo");
+                        log = "Perfil de trabajo existente para este usuario";
+                        Console.WriteLine(log);
                     }
+                    LogHelper.PublishLog("Creación perfil trabajo", log);
                 }
                 else
                 {
                     await EnviarMensajeCliente(manejoDataSocket, "Usuario inexistente para crear perfil de trabajo", "21");
-                    Console.WriteLine("Usuario inexistente para crear perfil de trabajo");
+                    log = "Usuario inexistente para crear perfil de trabajo";
+                    Console.WriteLine(log);
+                    LogHelper.PublishLog("Creación perfil trabajo", log);
                 }
             }
             catch (Exception e)
@@ -189,6 +195,7 @@ namespace GrpcServerProgram.Servidor
             PerfilTrabajo perfilUsuario = datosServidor.GetPerfilTrabajo(nombreUsuario);
             string codigo = "31";
             string mensaje = "Usuario existente";
+            string log = string.Empty;
 
             if (perfilUsuario == null)
             {
@@ -215,16 +222,21 @@ namespace GrpcServerProgram.Servidor
                 catch (Exception e)
                 {
                     await EnviarMensajeCliente(manejoDataSocket, e.Message, "00");
-                    Console.WriteLine("Ocurrio un error al recibir un archivo");
+                    log = "Ocurrió un error al recibir un archivo";
+                    Console.WriteLine(log);
+                    LogHelper.PublishLog("Asociación foto perfil", log);
                     return;
                 }
                 await EnviarMensajeCliente(manejoDataSocket, "El servidor recibio el archivo", "33");
-                Console.WriteLine("Se ha recibido un archivo");
-
+                log = "Se ha recibido un archivo";
+                Console.WriteLine(log);
+                LogHelper.PublishLog("Asociación foto perfil", log);
             }
             else
             {
-                Console.WriteLine("El usuario ingresado por el cliente no existe");
+                log = "El usuario ingresado por el cliente no existe";
+                Console.WriteLine(log);
+                LogHelper.PublishLog("Asociación foto perfil", log);
             }
         }
 
@@ -235,31 +247,25 @@ namespace GrpcServerProgram.Servidor
             Usuario usuarioLogIn = datosServidor.GetUsuario(datos[0]);
 
             string codigo = "030000";
+            string log = "LogIn rechazado";
 
             if (usuarioLogIn != null && usuarioLogIn.Password == datos[1])
             {
                 codigo = "020000";
-                usuarioLogIn.Conectado = true;
+                log = "LogIn exitoso";
             }
 
             try
             {
                 await manejoDataSocket.Send(codigo);
                 await manejoDataSocket.Send("");
+                LogHelper.PublishLog("LogIn", log);
             }
             catch (Exception e)
             {
                 Console.WriteLine($"Exception thrown - Message: {e.Message}");
                 throw;
             }
-        }
-
-        private static void Logout(string mensaje)
-        {
-            string[] datos = mensaje.Split(Constantes.CaracterSeparador);
-
-            Usuario usuarioLogIn = datosServidor.GetUsuario(datos[0]);
-            usuarioLogIn.Conectado = false;
         }
 
         private static async Task ConsultarPerfilesExistentes(ManejoStreamsHelper socketCliente, string mensajeUsuario)
@@ -309,7 +315,9 @@ namespace GrpcServerProgram.Servidor
                 respuestaUsuario = "\nNo se encontraron coincidencias\n";
             }
             await EnviarMensajeCliente(socketCliente, respuestaUsuario, "00");
-            Console.WriteLine("Se han buscado perfiles");
+            string log = "Se han buscado perfiles";
+            Console.WriteLine(log);
+            LogHelper.PublishLog("Consulta perfiles", log);
         }
 
         private static async Task ConsultarPerfilEspecifico(ManejoStreamsHelper socketCliente, string mensajeUsuario)
@@ -329,7 +337,9 @@ namespace GrpcServerProgram.Servidor
                 respuestaUsuario = "\nPerfil de trabajo no existente\n";
             }
             await EnviarMensajeCliente(socketCliente, respuestaUsuario, "98");
-            Console.WriteLine("Se ha buscado un perfil especifico");
+            string log = "Se ha buscado un perfil especifico";
+            Console.WriteLine(log);
+            LogHelper.PublishLog("Consulta perfil", log);
 
         }
 
@@ -347,7 +357,7 @@ namespace GrpcServerProgram.Servidor
                 return;
             }
 
-            if (perfil.Foto != string.Empty)
+            if (perfil.Foto != String.Empty)
             {
                 string pathApp = Directory.GetCurrentDirectory();
                 string absPath = Path.Combine(pathApp, perfil.Foto);
@@ -360,6 +370,10 @@ namespace GrpcServerProgram.Servidor
             {
                 await EnviarMensajeCliente(manejoDataSocket, "Este perfil de trabajo no tiene ninguna foto asociada", "54");
             }
+            
+            string log = "Solicitada imagen de perfil";
+            Console.WriteLine(log);
+            LogHelper.PublishLog("Enviar imagen perfil específico", log);
         }
 
         private static async Task DevolverListaUsuarios(ManejoStreamsHelper manejoDataSocket)
@@ -372,6 +386,10 @@ namespace GrpcServerProgram.Servidor
             }
 
             await EnviarMensajeCliente(manejoDataSocket, mensaje, "60");
+
+            string log = "Solicitada lista de usuarios";
+            Console.WriteLine(log);
+            LogHelper.PublishLog("Enviar lista de usuarios", log);
         }
 
         private static async Task DevolverListaNoLeidos(ManejoStreamsHelper manejoDataSocket, string nombreUsuario)
@@ -399,6 +417,10 @@ namespace GrpcServerProgram.Servidor
             }
 
             await EnviarMensajeCliente(manejoDataSocket, mensaje, "63");
+
+            string log = "Solicitada lista no leídos";
+            Console.WriteLine(log);
+            LogHelper.PublishLog("Enviar lista no leídos", log);
         }
 
         private static async Task DevolverHistorialChat(ManejoStreamsHelper manejoDataSocket, string cuerpo)
@@ -430,6 +452,10 @@ namespace GrpcServerProgram.Servidor
             }
 
             await EnviarMensajeCliente(manejoDataSocket, mensaje, "60");
+
+            string log = "Solicitado historial de chat entre " + usuarios[0] + " y " + usuarios[1];
+            Console.WriteLine(log);
+            LogHelper.PublishLog("Enviar lista no leídos", log);
         }
 
         private static void Mensajes(ManejoStreamsHelper socketCliente, string mensaje)
@@ -442,6 +468,10 @@ namespace GrpcServerProgram.Servidor
             chatActivo.ultimoEnHablar = contenido[0];
             chatActivo.visto = false;
             chatActivo.mensajes.Add(contenido[0] + " dice: " + contenido[2]);
+
+            string log = contenido[0] + " dice: " + contenido[2];
+            Console.WriteLine(log);
+            LogHelper.PublishLog("Enviar mensaje", log);
         }
 
         private static int ObtenerComando(string mensajeUsuario)
